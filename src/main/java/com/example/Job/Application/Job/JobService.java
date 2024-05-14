9d4774681c9276e9907d90e4e49fbd8a38d71801
Akash Dhingra <akashdhingra1996@gmail.com>
2024-05-13 20:51:46 -0400

    Added service and controller class

---
+++ src/main/java/com/example/Job/Application/Job/JobService.java
@@ -0,0 +1,10 @@
+package com.example.Job.Application.Job;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface JobService {
+    List<Job> findAll();
+    void createJob(Job job);
+
+}
---
+++ src/main/java/com/example/Job/Application/Job/impl/JobServiceImpl.java
@@ -0,0 +1,24 @@
+package com.example.Job.Application.Job.impl;
+
+import com.example.Job.Application.Job.Job;
+import com.example.Job.Application.Job.JobService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class JobServiceImpl implements JobService {
+
+    private List<Job> jobs = new ArrayList<>();
+
+    @Override
+    public List<Job> findAll() {
+        return jobs;
+    }
+
+    @Override
+    public void createJob(Job job) {
+        jobs.add(job);
+    }
+}
